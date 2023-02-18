8c99b59112d9e4e6cc102d2e1dffa49261dbf6a6
Camilo Nore <caminore22@gmail.com>
2023-02-18 17:45:12 -0500

    Conky added

---
+++ .conky/Alfirk/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+killall conky
+sleep 2s
+
+conky -c $HOME/.config/conky/Alfirk/Alfirk.conf &> /dev/null &
+conky -c $HOME/.config/conky/Alfirk/Alfirk2.conf &> /dev/null &
+conky -c $HOME/.config/conky/Alfirk/Alfirk3.conf &> /dev/null &
+
+exit
