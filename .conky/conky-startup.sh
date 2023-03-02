b8f95a4784228ee7269f13599e0b41f64afe5424
Camilo Nore <caminore22@gmail.com>
2023-03-02 12:07:57 -0500

    Updating dotfiles

--- .conky/conky-startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$DESKTOP_SESSION" = "cinnamon" ]; then 
-   sleep 20s
-   killall conky
-   cd "$HOME/.conky/Alfirk"
-   conky -c "$HOME/.conky/Alfirk/Alfirk.conf" &
-   exit 0
-fi
